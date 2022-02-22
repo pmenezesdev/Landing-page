fdc3ba90837432d5a6e86cb9595dfe7dd1603a41
jan Peto <63330174+janpeto@users.noreply.github.com>
2022-02-22 19:55:39 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+
+anime({
+    targets: '.cabecalho',
+    opacity: [0, 1],
+    duration: 1500,
+    easing: 'linear',
+});
+
+anime({
+    targets: '.conteudo',
+    opacity: [0, 1],
+    duration: 1500,
+    easing: 'linear',
+});
+
+
+anime({
+    targets: '.rodape',
+    opacity: [0, 1],
+    duration: 1500,
+    easing: 'linear',
+});
